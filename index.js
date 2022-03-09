1962f83c155cacd8b56f241f786749606affd3fe
Alejandro J <ajf@fastmail.com>
2022-03-09 12:08:42 -0600

    add website

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT;
 
-const port = 3300;
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials/');
 
 // serve static content
-app.use(
-  express.static('public', {
-    extensions: ['html'],
-  })
-);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: 'Alejandro Jimenez',
+    title: 'Node server',
+  });
+});
+app.get('/generic', (req, res) => {
+  res.render('generic');
+});
+app.get('/elements', (req, res) => {
+  res.render('elements');
+});
 
 app.get('*', (req, res) => {
-  res.sendFile('Hello World');
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port);
